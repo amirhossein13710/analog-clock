9d50feaa5305f490990a76f4d713bcbcbc4c18f4
amirhossein13710 <amirhossienradfar91@gmail.com>
2022-08-15 19:15:20 +0430

    analog clock

---
+++ app.js
@@ -0,0 +1,24 @@
+const secondsHand = document.querySelector('.secods-hand')
+const minutesHand = document.querySelector('.minutes-hand')
+const hoursHand = document.querySelector('.hours-hand')
+const data = document.querySelector('.data')
+
+function getTime(){
+    const now = new Date();
+    const seconds = now.getSeconds()
+    const minutes = now.getMinutes()
+    const hours = now.getHours()
+    const date = now.getDate()
+    console.log(date);
+
+    const timeInterval = 6
+    
+
+    secondsHand.style.transform = 'rotate(' + (seconds * timeInterval) + 'deg)'    
+    minutesHand.style.transform = 'rotate(' + (minutes * timeInterval + seconds / 10) + 'deg)'
+    hoursHand.style.transform = 'rotate(' + (hours * 30 + minutes / 2) + 'deg)'
+
+    data.innerText = date
+    
+}
+setInterval(getTime ,1000)
